81d4ed6c2ae42aab588196790eedd214e10ecb6b
MichaC <kletterer@web.de>
2017-08-01 07:52:52 +0200

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public (string a, string b) GetTuple()
+        {
+            return ("a", "b");
+        }
+
+        public IEnumerable<(string a, string b)> GetTuples()
+        {
+            yield return ("a", "b");
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using ClassLibrary1;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = new Class1().GetTuples().Select;
+        }
+    }
+}
